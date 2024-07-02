c65e1075eef61cf3c481c833a6ae717c52be4a7f
kennyAnyi9 <kennyanyi@gmail.com>
2024-07-02 02:42:04 +0000

    style: increase size of docs logo

--- cheatsheets/app/layout.config.tsx
+++ cheatsheets/app/layout.config.tsx
@@ -1,14 +1,12 @@
 import { type BaseLayoutProps, type DocsLayoutProps } from "fumadocs-ui/layout";
-import { BookIcon, LayoutTemplateIcon } from "lucide-react";
-import Image from "next/image";
 
 export const baseOptions: BaseLayoutProps = {
   githubUrl: "https://github.com/fuma-nama/fumadocs",
   nav: {
     title: (
       <>
-        <span className="font-medium [.uwu_&]:hidden max-md:[header_&]:hidden">
-          Cheatpedia
+        <span className="font-medium text-xl [.uwu_&]:hidden max-md:[header_&]:hidden">
+          ⌘ Cheatpedia
         </span>
       </>
     ),
